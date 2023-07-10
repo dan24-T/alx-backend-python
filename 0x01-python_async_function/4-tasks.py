6d4291b1b8f77f5063db6137137c5f31658c474f
dan24-T <106978779+dan24-T@users.noreply.github.com>
2023-07-10 21:29:41 +0300

    Update 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" The basics of async """
+""" async """
 
 import asyncio
 from typing import List
@@ -9,7 +9,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    spawn task_wait_random n times with the specified max_delay.
+    spawn
     """
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    return [await task for task in asyncio.as_completed(tasks)]
+    taskein = [task_wait_random(max_delay) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(taskein)]
